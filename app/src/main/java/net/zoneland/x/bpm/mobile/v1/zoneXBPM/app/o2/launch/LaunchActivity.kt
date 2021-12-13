d1d66153b676146a565852146db623750cb2cdd7
fancy <fancylou@outlook.com>
2021-12-13 17:25:29 +0800

    删除华为推送，不然无法修改包名

--- app/src/main/java/net/zoneland/x/bpm/mobile/v1/zoneXBPM/app/o2/launch/LaunchActivity.kt
+++ app/src/main/java/net/zoneland/x/bpm/mobile/v1/zoneXBPM/app/o2/launch/LaunchActivity.kt
@@ -21,8 +21,6 @@ import androidx.annotation.RequiresApi
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import cn.jpush.android.api.JPushInterface
-import com.huawei.hms.aaid.HmsInstanceId
-import com.huawei.hms.common.ApiException
 import kotlinx.android.synthetic.main.activity_launch.*
 import net.zoneland.x.bpm.mobile.v1.zoneXBPM.*
 import net.zoneland.x.bpm.mobile.v1.zoneXBPM.app.base.BaseMVPActivity
@@ -80,7 +78,7 @@ class LaunchActivity : BaseMVPActivity<LaunchContract.View, LaunchContract.Prese
     override fun afterSetContentView(savedInstanceState: Bundle?) {
         getJpushToken()
 
-        getHuaweiPushToken()
+//        getHuaweiPushToken()
     }
 
 
@@ -117,31 +115,31 @@ class LaunchActivity : BaseMVPActivity<LaunchContract.View, LaunchContract.Prese
     /**
      * 获取华为设备id
      */
-    private fun getHuaweiPushToken() {
-        val deviceId = O2SDKManager.instance().prefs().getString(O2.PRE_PUSH_HUAWEI_DEVICE_ID_KEY, "")
-        Log.e("LaunchActivity", "华为推送设备id  $deviceId")
-        if (TextUtils.isEmpty(deviceId)) {
-            // 创建一个新线程
-            object : Thread() {
-                override fun run() {
-                    try {
-                        // 从agconnect-service.json文件中读取appId
-                        val appId = "100016851"
-                        // 输入token标识"HCM"
-                        val tokenScope = "HCM"
-                        val token = HmsInstanceId.getInstance(this@LaunchActivity).getToken(appId, tokenScope)
-                        Log.i("LaunchActivity", "华为推送设备id: $token")
-                        // 判断token是否为空
-                        if (!TextUtils.isEmpty(token)) {
-                            sendRegTokenToServer(token)
-                        }
-                    } catch (e: ApiException) {
-                        Log.e("LaunchActivity", "华为推送设备id 获取失败, $e")
-                    }
-                }
-            }.start()
-        }
-    }
+//    private fun getHuaweiPushToken() {
+//        val deviceId = O2SDKManager.instance().prefs().getString(O2.PRE_PUSH_HUAWEI_DEVICE_ID_KEY, "")
+//        Log.e("LaunchActivity", "华为推送设备id  $deviceId")
+//        if (TextUtils.isEmpty(deviceId)) {
+//            // 创建一个新线程
+//            object : Thread() {
+//                override fun run() {
+//                    try {
+//                        // 从agconnect-service.json文件中读取appId
+//                        val appId = "100016851"
+//                        // 输入token标识"HCM"
+//                        val tokenScope = "HCM"
+//                        val token = HmsInstanceId.getInstance(this@LaunchActivity).getToken(appId, tokenScope)
+//                        Log.i("LaunchActivity", "华为推送设备id: $token")
+//                        // 判断token是否为空
+//                        if (!TextUtils.isEmpty(token)) {
+//                            sendRegTokenToServer(token)
+//                        }
+//                    } catch (e: ApiException) {
+//                        Log.e("LaunchActivity", "华为推送设备id 获取失败, $e")
+//                    }
+//                }
+//            }.start()
+//        }
+//    }
     private fun sendRegTokenToServer(token: String?) {
         Log.i("LaunchActivity", "sending token to server. token:$token")
         if (!TextUtils.isEmpty(token)) {
